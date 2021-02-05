37f8c62f18f9b9db126a18514b309340081e9512
Roberto Sanchez <rsanchez92@live.com>
2021-02-04 20:35:30 -0800

    Added model and controller

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character{Name = "Sam"}
+        };
+        [HttpGet("GetAll")]
+        public IActionResult Get(){
+            return Ok(characters);
+        }
+
+        public IActionResult GetSingle(){
+            
+        }
+
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace dotnet_rpg.Models
+{
+    public enum RpgClass
+    {
+        knight = 1,
+        mage = 2,
+        cleric = 3
+    }
+}
