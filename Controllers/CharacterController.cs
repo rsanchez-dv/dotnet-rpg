d446e4c68e67425b208fe37f880d8296acb1b86e
Roberto Sanchez <rsanchez92@live.com>
2021-02-04 21:30:47 -0800

    Added controllers

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,25 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
-    [Route("controller")]
+    [Route("[controller]")]
     public class CharacterController: ControllerBase
     {
         private static List<Character> characters = new List<Character>{
             new Character(),
-            new Character{Name = "Sam"}
+            new Character{Id = 1, Name = "Sam"}
         };
         [HttpGet("GetAll")]
         public IActionResult Get(){
             return Ok(characters);
         }
-
-        public IActionResult GetSingle(){
-            
+        
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id){
+            return Ok(characters.FirstOrDefault(c=> c.Id == id));
         }
-
     }
 }
