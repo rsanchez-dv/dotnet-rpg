4664989fb7d904511d8dc9e36841bf784cd01bb9
Roberto Sanchez <rsanchez2892@gmail.com>
2021-02-14 18:54:44 -0800

    Manage to read claims

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
@@ -22,7 +23,8 @@ namespace dotnet_rpg.Controllers
         
         [HttpGet("GetAll")]
         public async Task<IActionResult> Get(){
-            return Ok(await _characterService.GetAllCharacters());
+            int userId = int.Parse(User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value);
+            return Ok(await _characterService.GetAllCharacters(userId));
         }
         
         [HttpGet("{id}")]
